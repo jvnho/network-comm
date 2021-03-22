23379d4697d607fc267b3a8aca681927b2e1f00f
Nicolas Sangsiri <nicolassangsiri@gmail.com>
2021-03-22 11:21:31 +0100

    Sauvegarde

--- Message.java
+++ Message.java
@@ -1,3 +1,34 @@
 public class Message {
     
+    private int messageNumber;
+    private String code; //DIFF, OLDM, LAST, MESS...
+    private String originID;
+    private String message;
+
+    public Message(int messageNumber, String code, String originID, String message){
+        this.messageNumber = messageNumber;
+        this.code = code;
+        this.originID = originID;
+        this.message = message;
+    }
+
+    @Override public String toString(){
+        return this.code + " " + this.messageNumber + " " + this.originID + " " + this.message;
+    }
+
+    public int getMessageNumber(){
+        return this.messageNumber;
+    }
+
+    public String getCode(){
+        return this.code;
+    }
+
+    public String getOriginID(){
+        return this.originID;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
 }
--- Streamer.java
+++ Streamer.java
@@ -1,8 +1,13 @@
 import java.net.*;
 import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Streamer{
     
+    private static int LIST_SIZE = 1000;
+
     private String id;
     private int userPort;
     private String multicastIP;
@@ -10,14 +15,16 @@ public class Streamer{
     private String machineIP;
     private int msgIndex;
     private int tabIndex;
+    private List<Message> messageList;
 
-    public Streamer(String id, String multicastIP, int multicastPort, int userPort){
+    public Streamer(String id, String multicastIP, int multicastPort, int userPort, Message[] streamerMsg){
         this.id = id; //gestion erreur nombre de caractères
         this.userPort = userPort;
         this.multicastIP = multicastIP;
         this.multicastPort = multicastPort;
         this.msgIndex = 0;
         this.tabIndex = 0;
+        initMessageList(streamerMsg);
         try{
             this.machineIP = InetAddress.getLocalHost().getHostName();
         } catch(UnknownHostException e){
@@ -26,20 +33,50 @@ public class Streamer{
         }               
     }   
 
-    public void registerToManager(String managerAddr, int managerPort){
+    public void initMessageList(Message [] list){
+        this.messageList = new ArrayList<Message>();
+        for(int i = 0; i < list.length; i++){
+            this.messageList.add(list[i]);
+            this.tabIndex++;
+            this.msgIndex++;
+        }
+    }
 
+    public void registerToManager(String managerAddr, int managerPort){
+        try
+        {
+            Socket socket = new Socket(managerAddr, managerPort);
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+            String msg = "REGI " + this.id + " " + this.multicastIP + " " + this.multicastPort + " " 
+                + this.machineIP + " " + this.userPort;
+            pw.print(msg);
+            pw.flush();    
+            String recv = br.readLine();
+            System.out.println(recv);
+            pw.close();
+            br.close();
+            socket.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+            e.printStackTrace();
+        }
     }
 
     public synchronized void write(Message toWrite){
-
+        this.messageList.add(toWrite);
+        this.tabIndex = (this.tabIndex+1)%999;
+        this.msgIndex = (this.msgIndex+1)%9999;
     }
 
     public synchronized Message[] read(int n){
         return null;
     }
     
-    public synchronized void readRandom(){
-
+    public synchronized String readRandom(String code){
+        Message random = this.messageList.get(new Random().nextInt(this.tabIndex+1));
+        return new Message(random.getMessageNumber(), code, random.getOriginID(), random.getMessage()).toString();
     }
 
     @Override public String toString(){
@@ -86,9 +123,23 @@ public class Streamer{
             }
         
             @Override public void run(){
-                
+                try
+                {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    pw.print("HI\n");
+                    pw.flush();
+                    String mess=br.readLine();
+                    System.out.println("Message recu :"+mess);
+                    br.close();
+                    pw.close();
+                    socket.close();
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                    e.printStackTrace();
+                }
             }
         }
-        
     }
 }
