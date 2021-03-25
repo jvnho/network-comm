30de4023288372df3f79080b72738068c375ad67
RAPETERAMANA sarobidy <rapetera@informatique.univ-paris-diderot.fr>
2021-03-25 15:58:21 +0100

    verification of the format

---
+++ Format.java
@@ -0,0 +1,60 @@
+public class Format{
+    static int[] lenMessageRegi = new int[]{4, 8, 15, 4, 15, 6};
+    public static boolean isIpV4(String s){
+        //"127.000.000.001"
+        String[] content = s.split("\\.");
+        int len = content.length;
+        if(len != 4)return false;
+        for(int i= 0; i<len; i++){
+            try{
+                if(content[i].length() != 3)return false;
+                int numb = Integer.parseInt(content[i]);
+                if(numb<0 || numb>255)return false;
+            }catch(NumberFormatException e){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean isregi(String s){
+        //creer des class exection pour facilité les message d'erreur
+        System.out.println(s);
+        String []content = s.split(" ");
+        if(content.length != 6){
+            System.out.println("wrong spacing");
+            return false; //wrong spacing
+        }
+        int nbString = content.length;
+        for(int i=0; i<nbString; i++){
+            if(content[i].length() != lenMessageRegi[i]){
+                System.out.println("wrong size");
+                return false;//wrong size
+            }
+        }
+        if(content[0].equals("REGI")==false){
+            System.out.println("wrong begining");
+            return false; //wrong begining
+        }
+        if(content[nbString-1].charAt(4) != '\r' || content[nbString-1].charAt(5) != '\n'){
+            System.out.println("wrong ending");
+            return false;//wrong ending
+        }
+        try {
+            int port1 = Integer.parseInt(content[3]);
+            int port2 = Integer.parseInt(content[3].substring(0, 4));
+            if(port1<0 || port1>9999 || port2<0 || port2>9999){
+                System.out.println("invalid num port");
+                return false;//invalid numm port
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("port is not a integer");
+            //is not a integer
+            return false;
+        }
+        if(isIpV4(content[2]) == false || isIpV4(content[4]) == false){
+            System.out.println("wrong ipv4 format");
+            return false;
+        }
+        return true;
+    }
+}
--- StreamManager.java
+++ StreamManager.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class StreamManager{
     private ArrayList<Socket>listStreamerSocket;
     private ArrayList<String> listStreamer;
-    private int port;
+    private int port; 
     private int lenMax;
     private int currentLen;
     private int time_refresh;
@@ -18,7 +18,7 @@ public class StreamManager{
     public StreamManager(int port){
         this.listStreamerSocket = new ArrayList<Socket>();
         this.listStreamer = new ArrayList<String>();;
-        this.port = port;
+        this.port = port; //less than 9999
         this.lenMax = 99;
         this.currentLen = 0;
         this.time_refresh = 5;
@@ -46,22 +46,27 @@ public class StreamManager{
         public void run(){
             try{
                 BufferedReader reception = new BufferedReader(new InputStreamReader(soc.getInputStream()));//each message ends with \r\n
-                String message = reception.readLine(); //readline enleve le \n ?
-                //TODO si le message est de mauvais format
-                message = "ITEM"+ message.substring(4);//ends with \r ??
-                PrintWriter sending = new PrintWriter(new OutputStreamWriter(soc.getOutputStream()));
+                String message = reception.readLine(); //readline remove '\n' at the end ***?
+                message = message+"\n";//restore the '\n'
+                if(Format.isregi(message)){ //correct format
+                    message = "ITEM"+ message.substring(4);
+                    PrintWriter sending = new PrintWriter(new OutputStreamWriter(soc.getOutputStream()));
+                    synchronized(StreamManager.this){
+                        if(currentLen < lenMax){
+                            StreamManager.this.listStreamerSocket.add(soc);
+                            StreamManager.this.listStreamer.add(message);
+                            currentLen = currentLen + 1;
+                            sending.println("REOK\r");// \r\n
+                            sending.flush();
 
-                synchronized(StreamManager.this){
-                    if(currentLen < lenMax){
-                        StreamManager.this.listStreamerSocket.add(soc);
-                        StreamManager.this.listStreamer.add(message);
-                        currentLen = currentLen + 1;
-                        sending.println("REOK\r");// \r\n
-
-                    }else if(lenMax == currentLen){
-                        sending.println("RENO\r");
-                        soc.close();
+                        }else if(lenMax == currentLen){
+                            sending.println("RENO\r");
+                            sending.flush();
+                            soc.close();
+                        }
                     }
+                }else{
+                    soc.close();
                 }
             }catch(IOException e){
                 System.out.println("error SaveSocketStreamer");
@@ -91,15 +96,16 @@ public class StreamManager{
         public void run(){
             try{
                 BufferedReader reception = new BufferedReader(new InputStreamReader(soc.getInputStream()));//each message ends with \r\n
-                String message = reception.readLine(); //readline enleve le \n ?
+                String message = reception.readLine(); //readline enleve le \n  ****???
                 PrintWriter sending = new PrintWriter(new OutputStreamWriter(soc.getOutputStream()));
 
-                //TODO si le message est de format
                 if(message.equals("LIST\r")){
                     synchronized(StreamManager.this){
                         sending.println("LINB "+StreamManager.this.getCurrentLen()+"\r");// ? getCurrentLen is also synchronized check if don't block??
+                        sending.flush();
                         for (String streamer : listStreamer) {
-                            sending.println(streamer);
+                            sending.print(streamer);
+                            sending.flush();
                         }
                         soc.close();
                     }
@@ -118,9 +124,11 @@ public class StreamManager{
         String resutl = Integer.toString(this.currentLen);
         return (resutl.length()==1)?"0"+resutl:resutl;
     }
+    public void launchStreamManger(){
+        new Thread(this.new StreamerAccept()).start();//thread witch accept streamer and communicate with them
+        new Thread(this.new ClientAccept()).start();//thread witch accept all the client and communicate with them
+    }
     public static void main(String []args){
-        StreamManager manager = new StreamManager(Integer.parseInt(args[0]));
-        new Thread(manager.new StreamerAccept()).start();//thread witch accept streamer and communicate with them
-        new Thread(manager.new ClientAccept()).start();//thread witch accept all the client and communicate with them
+        //"REGI RADIO### 127.000.000.001 0120 127.000.000.001 0120\r\n"
     }
 }
