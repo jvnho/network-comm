1d6365334d6bfa5c7cd726db42a7534dd15df6bd
RAPETERAMANA sarobidy <rapetera@informatique.univ-paris-diderot.fr>
2021-04-02 21:39:39 +0200

    test

--- Test.java
+++ Test.java
@@ -31,16 +31,15 @@ public class Test{
     public static void main(String []args){
         try{
             //test diffuseur communique avec gestionnaire
-            /*Socket socket=new Socket("lulu",Integer.parseInt(args[0]));
+            Socket socket=new Socket("lulu",Integer.parseInt(args[0]));
             PrintWriter sender = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
             sender.println("REGI RADIO### 127.000.000.001 0120 127.000.000.001 0120\r");
             sender.flush();
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String recu = reader.readLine();
-            System.out.println(recu);*/
+            System.out.println(recu);
 
             //test client communique avec gestionnaire
-            Socket socket=new Socket("lulu",Integer.parseInt(args[0]));
             for(int i = 0; i<40; i++){
                 new Thread(new ClientThread(socket)).start();
             }
