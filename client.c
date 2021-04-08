8bcc111149100471a7a3d7cb6221793a52b9a2b2
RAPETERAMANA sarobidy <rapetera@informatique.univ-paris-diderot.fr>
2021-04-08 16:07:20 +0200

    static client

--- client.c
+++ client.c
@@ -91,13 +91,38 @@ int parseStreamer(infoStreamer *result, char *streamer){
 }
 //save a socket associated with a streamer
 int subscribe(char *streamer){
+    printf("debut\n");
+    int sock=socket(PF_INET,SOCK_DGRAM,0);
+    int ok=1;
+    int r=setsockopt(sock,SOL_SOCKET,SO_REUSEPORT,&ok,sizeof(ok));
+    struct sockaddr_in address_sock;
+    address_sock.sin_family=AF_INET;
+    address_sock.sin_port=htons(4999);
+    address_sock.sin_addr.s_addr=htonl(INADDR_ANY);
+    r=bind(sock,(struct sockaddr *)&address_sock,sizeof(struct sockaddr_in));
+    struct ip_mreq mreq;
+    mreq.imr_multiaddr.s_addr=inet_addr("225.010.020.030");
+    mreq.imr_interface.s_addr=htonl(INADDR_ANY);
+    r=setsockopt(sock,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq));
+    char tampon[100];
+    while(1){
+        int rec=recv(sock,tampon,100,0);
+        tampon[rec]='\0';
+        printf("Message recu : %s\n",tampon);
+    }
+    return 0;
+    /*
     printf("%s\n", streamer);
     //parse the streamer buffer
     infoStreamer result;
     memset(&result, 0, sizeof(infoStreamer));
     if(parseStreamer(&result, streamer)==-1)return -1;
     printf("port multi = %d\n", result.multicastPort);
-    printf("ip multi = %s\n", result.multicatIP);
+    printf("ip multi = -%s-\n", result.multicatIP);
+
+
+    
+    
     //create the soc
     int sock=socket(PF_INET, SOCK_DGRAM, 0);
     if(sock == -1){
@@ -138,6 +163,8 @@ int subscribe(char *streamer){
         printf("Message recu : %s\n",tampon);
     }
     return sock;
+    */
+    return 0;
 }
 int main(void){
     char test[130] = "ITEM diffprof 225.010.020.030 4999 192.168.070.236 5999\r\n";
